14cf7eed7dc8c7a19655d71923bd92e41e104043
shoman <shoma216@gmail.com>
2019-10-16 23:18:34 +0200

    abc088 b

---
+++ abc088/b/go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(a)))
+	alice, bob := 0, 0
+	for i, v := range a {
+		if i%2 == 0 {
+			alice += v
+		} else {
+			bob += v
+		}
+	}
+	fmt.Println(alice - bob)
+}
