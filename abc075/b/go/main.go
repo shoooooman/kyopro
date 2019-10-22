45e7d0c61b3db9493901a2b217b611ff310297e3
shoman <shoma216@gmail.com>
2019-10-22 23:53:23 +0200

    abc075 b

---
+++ abc075/b/go/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	var h, w int
+	fmt.Scan(&h, &w)
+
+	status := make([][]rune, h)
+	for i := 0; i < h; i++ {
+		var s string
+		fmt.Scan(&s)
+		status[i] = make([]rune, w)
+		for j, c := range s {
+			status[i][j] = c
+		}
+	}
+
+	count := make([][]int, h)
+	for i := 0; i < h; i++ {
+		count[i] = make([]int, w)
+	}
+	x := []int{-1, 0, 1}
+	y := []int{-1, 0, 1}
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			cnt := 0
+			for _, dx := range x {
+				for _, dy := range y {
+					if dx == 0 && dy == 0 {
+						continue
+					}
+					if i+dx >= 0 && i+dx < h &&
+						j+dy >= 0 && j+dy < w &&
+						status[i+dx][j+dy] == '#' {
+						cnt++
+					}
+				}
+			}
+			count[i][j] = cnt
+		}
+	}
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if status[i][j] == '#' {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf("%d", count[i][j])
+			}
+		}
+		fmt.Println()
+	}
+}
