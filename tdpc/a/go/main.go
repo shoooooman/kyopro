69d80efea3d9942cbc1152dc3d31058d61c0fc38
shoman <shoma216@gmail.com>
2019-10-28 19:21:06 +0100

    tdpc a

---
+++ tdpc/a/go/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	p := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&p[i])
+	}
+
+	ans := 0
+	dp := make(map[int]bool)
+	dp[0] = true
+	for i := 0; i < n; i++ {
+		for sum := 10000; sum >= 0; sum-- {
+			if _, ok := dp[sum]; ok {
+				dp[sum+p[i]] = true
+			}
+		}
+	}
+
+	for i := 0; i <= 10000; i++ {
+		if _, ok := dp[i]; ok {
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
