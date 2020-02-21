5c1a17ea474f13271aac57ee6dc94b019464cb60
shoman <shoma216@gmail.com>
2020-02-21 09:07:19 +0100

    cracking 2.4

---
+++ cracking/problems/2.4/go/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shoooooman/kyopro/cracking/data_structures/mylist"
+)
+
+/* xより小さい数は先頭から挿入していく
+ * 挿入する場所を挿入するたびにずらしていく
+ */
+func main() {
+	const n = 10
+	list := mylist.GenLinkedList(n)
+	list.Show()
+
+	var x int
+	fmt.Scan(&x)
+
+	halfTop := 0
+	runner := list.Head
+	count := 0
+	for runner != nil {
+		if runner.Val < x {
+			rv := runner.Val
+			list.Remove(count)
+			fmt.Printf("Removed %d at %d\n", rv, count)
+			list.InsertBefore(halfTop, &mylist.Node{rv, nil})
+			halfTop++
+			fmt.Println(halfTop)
+			list.Show()
+		}
+		runner = runner.Next
+		count++
+	}
+	list.Show()
+}
