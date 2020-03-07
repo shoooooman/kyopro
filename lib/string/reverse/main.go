6df06bd367ade98c52132b6f58094d3372106bf4
shoman <shoma216@gmail.com>
2020-03-07 15:02:51 +0100

    lib string/reverse

---
+++ lib/string/reverse/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func reverse(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
+
+func main() {
+	str := "abcde"
+	fmt.Println(reverse(str)) // edcba
+}
