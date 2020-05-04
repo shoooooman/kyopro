5c519f4c06f385f7e327dfa382a6e81af84f99c1
shoman <shoma216@gmail.com>
2020-05-04 11:33:32 +0900

    lib math/int/minmax; add minmax for arbitrary length

--- lib/math/int/minmax/main.go
+++ lib/math/int/minmax/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func min(x, y int) int {
 	if x <= y {
@@ -16,8 +19,30 @@ func max(x, y int) int {
 	return y
 }
 
+func minInts(values ...int) int {
+	mv := math.MaxInt64
+	for _, v := range values {
+		if v < mv {
+			mv = v
+		}
+	}
+	return mv
+}
+
+func maxInts(values ...int) int {
+	mv := math.MinInt64
+	for _, v := range values {
+		if v > mv {
+			mv = v
+		}
+	}
+	return mv
+}
+
 func main() {
 	x, y := 1, 2
 	fmt.Println(min(x, y))
 	fmt.Println(max(x, y))
+	fmt.Println(minInts(1, 2, 3))
+	fmt.Println(maxInts(1, 2, 3))
 }
