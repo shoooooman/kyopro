d30a5ab1fec22201ffefae0fa67e9225f914e2ec
shoooooman <shoma216@gmail.com>
2020-05-11 14:09:46 +0900

    abc167 e

--- atcoder/abc/abc167/e/go/main.go
+++ atcoder/abc/abc167/e/go/main.go
@@ -2,24 +2,58 @@ package main
 
 import "fmt"
 
-const mod = 998244353
+const (
+	mod   = 998244353
+	limit = 200001
+)
 
-func dfs(n, index, m, remain int) int {
-	if index == n {
+var (
+	fac  = make([]int, limit)
+	finv = make([]int, limit)
+	inv  = make([]int, limit)
+)
+
+func powMod(a, n int) int {
+	if n == 0 {
 		return 1
 	}
-
-	sum := (dfs(n, index+1, m, remain) % mod * (m - 1)) % mod
-	if remain > 0 {
-		sum += dfs(n, index+1, m, remain-1) % mod
+	if n%2 == 0 {
+		d := powMod(a, n/2) % mod
+		return d * d % mod
 	}
-	return sum
+	return a * powMod(a, n-1) % mod
+}
+
+func combModInit() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i < limit; i++ {
+		fac[i] = fac[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod
+		finv[i] = finv[i-1] * inv[i] % mod
+	}
+}
+
+func combMod(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % mod) % mod
 }
 
 func main() {
 	var n, m, k int
 	fmt.Scan(&n, &m, &k)
 
-	ans := m * dfs(n, 1, m, k) % mod
+	combModInit()
+	ans := 0
+	for i := 0; i <= k; i++ {
+		ans += (((m * (combMod(n-1, i) % mod)) % mod) * powMod(m-1, n-i-1)) % mod
+		ans %= mod
+	}
 	fmt.Println(ans)
 }
