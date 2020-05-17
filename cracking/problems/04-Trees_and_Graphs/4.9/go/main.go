1d8e05ef2a382316fce76f7b286e037649e975f5
shoooooman <shoma216@gmail.com>
2020-05-17 16:36:34 +0900

    cracking 4.9; fix solution

--- cracking/problems/04-Trees_and_Graphs/4.9/go/main.go
+++ cracking/problems/04-Trees_and_Graphs/4.9/go/main.go
@@ -6,57 +6,61 @@ import (
 	"github.com/shoooooman/kyopro/lib/data_structures/mybst"
 )
 
-func appendLists(orders [][]int, lists [][]int) [][]int {
-	joined := make([][]int, len(orders)*len(lists))
-	k := 0
-	for i := range orders {
-		for j := range lists {
-			joined[k] = append(orders[i], lists[j]...)
-			k++
+func weaveLists(left, right, prefix []int, weaved *[][]int) {
+	if len(left) == 0 || len(right) == 0 {
+		// weavedにはprefixのポインタが格納されるので
+		// コピーしないと呼び出し元でprefixを変更したときに値が壊れる
+		cp := make([]int, len(prefix))
+		copy(cp, prefix)
+		cp = append(cp, left...)
+		cp = append(cp, right...)
+		*weaved = append(*weaved, cp)
+		return
+	}
+
+	leftHead := left[0]
+	prefix = append(prefix, leftHead)
+	weaveLists(left[1:], right, prefix, weaved)
+	prefix = prefix[:len(prefix)-1]
+
+	rightHead := right[0]
+	prefix = append(prefix, rightHead)
+	weaveLists(left, right[1:], prefix, weaved)
+	prefix = prefix[:len(prefix)-1]
+}
+
+func doubleDFS(node *mybst.Node) [][]int {
+	result := make([][]int, 0)
+
+	if node == nil {
+		// 要素を入れておくことで下の
+		// left, rightについてのループが回る
+		result = append(result, make([]int, 0))
+		return result
+	}
+
+	prefix := []int{node.Val}
+
+	left := doubleDFS(node.Left)
+	right := doubleDFS(node.Right)
+	for _, ll := range left {
+		for _, rl := range right {
+			weaved := make([][]int, 0)
+			weaveLists(ll, rl, prefix, &weaved)
+			result = append(result, weaved...)
 		}
 	}
-	return joined
-}
-
-func appendAll(list []int, orders [][]int) [][]int {
-	joined := make([][]int, len(orders))
-	for i := range orders {
-		joined[i] = append(list, orders[i]...)
-	}
-	return joined
-}
-
-func doubleDFS(node *mybst.Node, orders [][]int) [][]int {
-	if node == nil {
-		return nil
-	}
-
-	left := doubleDFS(node.Left, orders)
-	right := doubleDFS(node.Right, orders)
-
-	self := []int{node.Val}
-	if left == nil && right == nil {
-		return [][]int{self}
-	} else if left != nil && right == nil {
-		return appendAll(self, left)
-	} else if right != nil && left == nil {
-		return appendAll(self, right)
-	}
-	ltor := appendLists(left, right)
-	rtol := appendLists(right, left)
-	next1 := appendAll(self, ltor)
-	next2 := appendAll(self, rtol)
-	return append(next1, next2...)
+	return result
 }
 
 func solve(bst *mybst.Tree) [][]int {
-	orders := make([][]int, 0)
-	return doubleDFS(bst.Root, orders)
+	return doubleDFS(bst.Root)
 }
 
 func main() {
-	values := []int{1, 2, 3, 4, 5, 6, 7}
+	// values := []int{1, 2, 3, 4, 5, 6, 7}
 	// values := []int{1, 2, 3}
+	values := []int{1, 2, 3, 4}
 	bst := mybst.GenBST(values)
 	bst.PrintTree()
 	mybst.PrintValues(bst.Root)
