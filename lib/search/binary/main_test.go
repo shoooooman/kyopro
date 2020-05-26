b100a959e6cddf8a9facbbd488d725a1aaeda470
shoooooman <shoma216@gmail.com>
2020-05-26 22:48:33 +0900

    lib search/binary; add test cases

--- lib/search/binary/main_test.go
+++ lib/search/binary/main_test.go
@@ -20,6 +20,8 @@ func TestBinarySearch(t *testing.T) {
 		{"test9", []int{1, 2}, 1, 0},
 		{"test10", []int{1, 2}, 2, 1},
 		{"test11", []int{1, 2}, 0, -1},
+		{"test12", []int{1, 2, 2, 4, 5}, 2, 2},
+		{"test13", []int{1, 2, 2, 3, 4, 5, 6}, 2, 1},
 	}
 
 	for _, tt := range tests {
